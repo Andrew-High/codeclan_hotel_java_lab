5d8a55cd34261bcc0ddbe8ff91d360a7c13f35e3
Andrew High <andrewhigh4@hotmail.com>
2021-06-10 11:48:08 +0100

    Added guest class

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,9 +1,15 @@
-public class Room {
+import java.util.ArrayList;
+
+public abstract class Room {
 
     private RoomType roomType;
+    private int capacity;
+    private ArrayList<Guest> numberOfGuests;
 
-    public Room(RoomType roomType){
+    public Room(RoomType roomType, int capacity, ArrayList numberOfGuests){
         this.roomType = roomType;
+        this.capacity = capacity;
+        this.numberOfGuests = numberOfGuests;
     }
     public RoomType getRoomType(){
         return this.roomType;
