6fb26beeda205eb6047fc4b26f8749e4a045d557
Andrew High <andrewhigh4@hotmail.com>
2021-06-10 11:00:15 +0100

    end of lesson start of lab

---
+++ src/test/java/RoomTest.java
@@ -0,0 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RoomTest {
+
+    Room room;
+
+    @Before
+    public void before(){
+        room = new Room(RoomType.DOUBLE);
+    }
+
+    @Test
+    public void canGetRoomType(){
+        assertEquals(RoomType.DOUBLE, room.getRoomType());
+    }
+
+    @Test
+    public void doublehasValue2(){
+        assertEquals(2, room.getValueFromEnum());
+    }
+
+//    @Test
+//    public void roomTypeTypo(){
+//        Room newRoom = new Room("Dooble");
+//        assertEquals("Dooble", newRoom.getRoomType());
+//    }
+//
+//    @Test
+//    public void roomCanBeBananas(){
+//        Room bananaRoom = new Room("banana");
+//        assertEquals("banana", bananaRoom.getRoomType());
+//    }
+}
